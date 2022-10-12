ae84f7e2eb23d68a6c494116f8007843786d004f
DhruvKhambhata <khambhatadhruv28@gmail.com>
2022-10-12 13:06:06 +0530

    updated Ready

--- dhruv/project/Ready/js/reveal.js
+++ dhruv/project/Ready/js/reveal.js
@@ -15,21 +15,4 @@ function reveal() {
   window.addEventListener("scroll", reveal);
 
 
-  function onScroll(){
-    var textfocus = document.querySelectorAll(".textfocus");
-
-    for (var i = 0; i < textfocus.length; i++) {
-        var windowHeight = window.innerHeight;
-        var elementTop = textfocus[i].getBoundingClientRect().top;
-        var elementVisible = 150;
-    
-        if (elementTop < windowHeight - elementVisible) {
-            textfocus[i].classList.add("active");
-        } else {
-            textfocus[i].classList.remove("active");
-        }
-      }
-    }
-    
-    window.addEventListener("scroll", textfocus);
   
